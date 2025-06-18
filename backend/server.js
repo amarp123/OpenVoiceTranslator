3aeacd7295ead80803ae7a1eaf39bdef917b8e4d
Amarp123 <amarpujari800@gmail.com>
2025-06-18 14:16:54 +0530

    vercel.json

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,12 @@ app.post('/translate', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3001;
+app.get('/',(req,res)=>{
+  res.send({
+    activeStauts:true,
+    erroe:false,
+  })
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
